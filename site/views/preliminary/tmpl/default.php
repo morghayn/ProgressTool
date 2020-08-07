f3889ddd145ee9ff587a142649032359db2948e6
Morgan <morgan.nolan@hotmail.com>
2020-08-07 22:40:03 +0100

    [Development]: Project Activation design change.

--- site/views/preliminary/tmpl/default.php
+++ site/views/preliminary/tmpl/default.php
@@ -1,23 +1,44 @@
 <?php defined('_JEXEC') or die; ?>
 
-<!-- TODO -- Edit CSS -- Add more info button -- Check if all buttons have been checked via JavaScript -->
+<!-- TODO -- Check if all buttons have been checked via JavaScript -->
 
-<?php foreach ($this->questions as $question): ?>
-    <div class="box">
-        <div class="page_toggle">
-            <label class="toggle">
-                <input class="toggle_input" type="checkbox">
-                <span class="toggle_label">
-                        <span class="toggle_text"><?php echo $question->question; ?></span>
-                    </span>
-            </label>
+
+<!--
+<p>
+    If you have any uncertainties to any of these questions... it's highly recommend to click the helpful resources
+    button
+</p>
+-->
+
+<?php $colour = "#ff6666"; ?>
+
+<!-- Question Box -->
+<div class="projectActivationContainer" style="border-color: <?php echo $colour; ?>">
+
+    <!-- Title -->
+    <div class="titleContainer" style="background-color: <?php echo $colour; ?>;">
+        <div class="title">
+            <?php echo "Project Activation"; ?>
         </div>
     </div>
-<?php endforeach; ?>
 
-<!-- TODO change names -->
-<div class="button-box">
-    <button href="something" class="test">Help</button>
-    <div class="space"></div>
-    <button href="something" class="test" disabled>Verify Project</button>
+    <!-- Questions -->
+    <div class="questionsContainer">
+        <?php foreach ($this->questions as $question): ?>
+            <label class="questionContainer"
+                   style="--outlineColour: <?php echo $colour; ?>; --choiceColour: <?php echo $colour; ?>;">
+                <input class="questionInput" type="checkbox">
+                <span class="questionLabel">
+                    <span class="question">
+                        <?php echo $question->question; ?>
+                    </span>
+                </span>
+            </label>
+        <?php endforeach; ?>
+    </div>
+
+    <div class="buttonBox">
+        <button href="something" class="button">helpful resources</button>
+        <button href="something" class="button" disabled>activate project</button>
+    </div>
 </div>
