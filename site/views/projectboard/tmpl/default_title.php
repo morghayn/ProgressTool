ff96f3609ebde3b0277552f852451c456d5d04fb
Morgan <morgan.nolan@hotmail.com>
2020-08-31 21:16:16 +0100

    [Development]: Project Board heading improvement
    * css names are temporary for this CSS modification
    (I intend to do a mass CSS clean-up in the coming days)
    
    + added an introduction paragraph provided by my co-worker. I intend to add others that have been provided to me for other parts of the tool.

--- site/views/projectboard/tmpl/default_title.php
+++ site/views/projectboard/tmpl/default_title.php
@@ -1,9 +1,16 @@
 <?php defined('_JEXEC') or die; ?>
 
-<div class="masterChest">
-    <div class="titleChest">
-        <div class="cornerBlank"></div>
-        <div class="title">ProjectBoard</div>
-        <button onclick="window.location = '?option=com_progresstool&view=projectcreate'" class="cornerButton"></button>
+<div class="headC">
+
+    <div class="titlC">
+        <div class="titl">
+            Project Board
+        </div>
     </div>
+
+    <button onclick="window.location = '?option=com_progresstool&view=projectcreate'" class="buttonCreate">
+
+    </button>
+
 </div>
+
