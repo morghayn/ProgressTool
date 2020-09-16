dc84814be01ab9a63d2f4d3404f163e94dd897dc
Morgan <morgan.nolan@hotmail.com>
2020-09-17 00:22:17 +0100

    [Dev]: Beginning of the QuestionEditor

---
+++ admin/views/imagetest/tmpl/default_question.php
@@ -0,0 +1,41 @@
+<?php
+
+defined('_JEXEC') or die;
+
+$questionCounter = $this->questionCounter;
+$question = $this->question->question;
+$questionID = $this->question->id;
+$colourHex = $this->question->colour_hex;
+
+?>
+
+<div class="flexTest" style="position: relative; width: 60%; margin: 0 auto;">
+    <div id="iconChest<?php echo $this->question->id; ?>" class="iconChest" style="background-color: red; position: absolute; width: 200px; height: 200px; bottom: 0; right: 0;">
+        <!--
+        <figure style="width:100%; height:100%; margin:0;">
+        <img src="../images/com_progresstool/survey/Illustrations_InitiativesLocale02.jpg" alt="yo not loading">
+        </figure>
+        -->
+    </div>
+
+    <div class="masterChest" style="border-color: <?php echo $colourHex; ?>; margin-bottom: 0;">
+
+        <div class="masterChestHeadingChest" style="background-color: <?php echo $colourHex; ?>;">
+            <h2 class="masterChestHeading" style="text-align: left; width: 75%;">
+                <?php echo $questionCounter . '. ' . $question; ?>
+            </h2>
+        </div>
+
+        <div class="optionsChest">
+
+            <?php
+            foreach ($this->choices[$questionID] as $this->choice):
+                echo $this->loadTemplate('option');
+            endforeach;
+            ?>
+
+        </div>
+
+
+    </div>
+</div>
