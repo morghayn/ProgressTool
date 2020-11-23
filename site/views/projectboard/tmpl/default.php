86ebf6a4dcdc7659984e5303d42aee00ef6d6bd9
Morgan Nolan <morgan.nolan@hotmail.com>
2020-11-23 20:21:28 +0000

    Refactored main Project Board template.

--- site/views/projectboard/tmpl/default.php
+++ site/views/projectboard/tmpl/default.php
@@ -1,38 +1,39 @@
-<?php defined('_JEXEC') or die; ?>
+<?php defined('_JEXEC') or die;
 
-    <input id="token" type="hidden" name="<?php echo JSession::getFormToken(); ?>" value="1"/>
-
-<?php
-
+// Title container
 echo $this->loadTemplate('title');
 
-if (!$this->projects):
+if ($this->projects): // If user has projects... ?>
 
-    echo $this->loadTemplate('noprojects');
+    <div id="projectBox">
+        <!-- For displaying a selected project -->
+    </div>
 
-else:
-
-    echo '<div id="projectBox"></div>';
-
-    echo '<div id="projectsBox">';
-
+    <div id="projectsBox">
+        <?php
         $this->projectCount = 0;
         foreach ($this->projects as $this->project):
             $this->projectCount++;
+            echo $this->project->activated == 1 ? $this->loadTemplate('active') : $this->loadTemplate('inactive');
+        endforeach; ?>
+    </div>;
 
-            if ($this->project->activated == 1)
-            {
-                echo $this->loadTemplate('active');
-            }
-            else
-            {
-                echo $this->loadTemplate('inactive');
-            }
+<?php else: // If user does not have projects... ?>
 
-        endforeach;
+    <p class="introductionParagraph" style="margin-top: 50px;">
+        Welcome to the Progress Tool. This tool will use a survey to measure what stage of development your Community
+        Energy project is at and will provide guidance on what next steps to take.<br><br>
 
-    echo '</div>';
+        To begin, click the green button on the top right corner to create a project entry. After adding details about the project and selecting which
+        type of Renewable Energy it employs click “Submit”. The tool will save your project details for future updates.<br><br>
 
-endif;
+        The survey consists of 26 questions relating to different stages of a Community Energy project. Please select all options that apply to your
+        project, feel free to choose more than one option.<br><br>
 
-?>
+        The score tallied from the survey responses is used to populate a spider diagram, describing the progress under each heading from the
+        <a href="/timeline">ECCO timeline</a> (<b class="people">People</b>, <b class="technology">Technology</b> and <b class="finance">Finance</b>).
+    </p>
+
+<?php endif; ?>
+
+<input id="token" type="hidden" name="<?php echo JSession::getFormToken(); ?>" value="1"/>
