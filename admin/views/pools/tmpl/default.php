dd990dabc44d41318b7ab995e4771e16406bf91c
Morgan Nolan <morgan.nolan@hotmail.com>
2020-12-01 15:40:02 +0000

    Removed pools redirect. Will be moved to dashboard.

--- admin/views/pools/tmpl/default.php
+++
@@ -1,9 +0,0 @@
-<?php defined('_JEXEC') or die; ?>
-
-<div class="fragmentContainer">
-    <?php foreach ($this->pools as $pool): ?>
-        <a href="?option=com_progresstool&view=pool&pool=<?php echo $pool->id; ?>">
-            <?php echo $pool->country; ?>
-        </a>
-    <?php endforeach; ?>
-</div>
