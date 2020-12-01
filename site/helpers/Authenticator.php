603d6b761edc250821da38ec8533c45e8ff76a80
Morgan Nolan <morgan.nolan@hotmail.com>
2020-12-01 17:38:49 +0000

    Refactored Authenticator helper.

--- site/helpers/Authenticator.php
+++ site/helpers/Authenticator.php
@@ -3,12 +3,50 @@
 
 abstract class Authenticator
 {
+    public static function authenticate($projectID)
+    {
+        $genericErrorMessage = 'Project authentication failed. Please contact the sites administrator.';
+        $user = JFactory::getUser();
+
+        // Redirect if guest
+        self::redirectGuests();
+
+        if (!self::projectExists($projectID))
+        {
+            // Redirect if project does not exist
+            JFactory::getApplication()->redirect(
+                JRoute::_('index.php?option=com_progresstool&view=projectboard', $genericErrorMessage), $genericErrorMessage
+            );
+        }
+        elseif (!self::hasAccess($user->id, $projectID))
+        {
+            // Redirect if user does not have permission to access
+            JFactory::getApplication()->redirect(
+                JRoute::_('index.php?option=com_progresstool&view=projectboard', $genericErrorMessage), $genericErrorMessage
+            );
+        }
+    }
+
     /**
-     * Returns a boolean to indicate whether a project actually exists. True means it does and false means it does not.
+     * Redirects guests.
+     */
+    public static function redirectGuests()
+    {
+        if (JFactory::getUser()->get('guest'))
+        {
+            $return = urlencode(base64_encode('index.php?option=com_progresstool&view=projectboard'));
+            JFactory::getApplication()->redirect(
+                'index.php?option=com_users&view=login&return=' . $return,
+                'You must be logged in to use the Progress Tool.'
+            );
+        }
+    }
+
+    /**
+     * Returns true if project exists, returns false if it cannot be found
      *
-     * @param int $projectID the ID of the project in which a check will be done.
-     * @return bool to indicate whether project exists.
-     * @since 0.5.0
+     * @param int $projectID
+     * @return bool
      */
     public static function projectExists($projectID)
     {
@@ -23,11 +61,11 @@ abstract class Authenticator
     }
 
     /**
-     * Checks if user should have access to the project
+     * Returns true if user has permission, returns false if user does not.
      *
-     * @param int $userID the ID of the user
-     * @param int $projectID the ID of the project.
-     * @since 0.5.0
+     * @param int $userID
+     * @param int $projectID
+     * @return bool
      */
     public static function hasAccess($userID, $projectID)
     {
@@ -35,67 +73,23 @@ abstract class Authenticator
         $checkAccess = $db->getQuery(true);
 
         // conditions for the query.
-        $where =
-            array(
-                $db->quoteName('P.id') . ' = ' . $db->quote($projectID),
-                $db->quoteName('P.user_id') . ' = ' . $db->quote($userID)
-            );
-        $orwhere =
-            array(
-                $db->quoteName('P.id') . ' = ' . $db->quote($projectID),
-                $db->quoteName('CGM.memberid') . ' = ' . $db->quote($userID),
-                $db->quoteName('CGM.permissions') . ' = 1'
-
-            );
+        $where = array(
+            $db->quoteName('P.id') . ' = ' . $db->quote($projectID),
+            $db->quoteName('P.user_id') . ' = ' . $db->quote($userID)
+        );
+        $orWhere = array(
+            $db->quoteName('P.id') . ' = ' . $db->quote($projectID),
+            $db->quoteName('CGM.memberid') . ' = ' . $db->quote($userID),
+            $db->quoteName('CGM.permissions') . ' = 1'
+        );
 
         $checkAccess
             ->select('IF(COUNT(P.id) < 1, 0, 1) AS status')
             ->from($db->quoteName('#__pt_project', 'P'))
             ->leftjoin($db->quoteName('#__community_groups_members', 'CGM') . ' ON ' . $db->quoteName('P.group_id') . ' = ' . $db->quoteName('CGM.groupid'))
             ->where($where)
-            ->orwhere($orwhere);
+            ->orwhere($orWhere);
 
-        //-- 1 = access granted //-- 0 = access denied
         return $db->setQuery($checkAccess)->loadResult() == 1;
-    }
-
-    public static function redirectGuests()
-    {
-        $user = JFactory::getUser();
-        $guestRedirectMessage = 'You must be logged in to use the Progress Tool.';
-
-        // If user is guest.
-        if ($user->get('guest'))
-        {
-            $return = urlencode(base64_encode('index.php?option=com_progresstool&view=projectboard'));
-            JFactory::getApplication()->redirect(
-                'index.php?option=com_users&view=login&return=' . $return,
-                $guestRedirectMessage
-            );
-        }
-    }
-
-    public static function authenticate($projectID)
-    {
-        $genericErrorMessage = 'Project authentication failed. Please contact the sites administrator.';
-        $user = JFactory::getUser();
-
-        // If user is guest.
-        self::redirectGuests();
-
-        // If project does not exist.
-        if (!self::projectExists($projectID))
-        {
-            JFactory::getApplication()->redirect(
-                JRoute::_('index.php?option=com_progresstool&view=projectboard', $genericErrorMessage), $genericErrorMessage
-            );
-        }
-        // If user should does not have rights to access the project.
-        elseif (!self::hasAccess($user->id, $projectID))
-        {
-            JFactory::getApplication()->redirect(
-                JRoute::_('index.php?option=com_progresstool&view=projectboard', $genericErrorMessage), $genericErrorMessage
-            );
-        }
     }
 }
