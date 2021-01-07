a610301dad3ffa3fd559d7342b933884243f8c27
Morgan Nolan <morgan.nolan@hotmail.com>
2021-01-07 21:31:05 +0000

    Altered openNav() logic.

--- media/js/admin/adminBase.js
+++ media/js/admin/adminBase.js
@@ -1,7 +1,17 @@
 function openNav()
 {
-    document.getElementById("sideNavigation").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    let currentWidth = document.getElementById("sideNavigation").style.width;
+
+    if (currentWidth !== '250px')
+    {
+        document.getElementById("sideNavigation").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+    }
+    else
+    {
+        document.getElementById("sideNavigation").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+    }
 }
 
 function closeNav()
