585d51ce72862fa3b9e9e254f54b9c69595370a6
Morgan Nolan <morgan.nolan@hotmail.com>
2021-01-14 23:48:30 +0000

    Updated projects model to reflect previous commits.
    *Added function getProject()
    *Refactored getProjects() so the columns are actually specified, opposed to grabbing all columns.

--- admin/models/projects.php
+++ admin/models/projects.php
@@ -18,16 +18,61 @@ class ProgressToolModelProjects extends JModelLegacy
      * Returns all projects on record.
      *
      * @return object list comprising of all projects on record.
+     * @since 0.5.0
      */
     public function getProjects()
     {
         $db = JFactory::getDbo();
         $getProjects = $db->getQuery(true);
 
+        $columns = array(
+            'P.id',
+            'P.user_id',
+            'P.group_id',
+            'P.name',
+            'P.description',
+            'P.type_id',
+            'P.creation_date',
+            'P.activated',
+            //'U.name',
+            'U.username',
+            'U.email'
+        );
+
         $getProjects
-            ->select('*')
-            ->from($db->quoteName('#__pt_project'));
+            ->select($columns)
+            ->from($db->quoteName('#__pt_project', 'P'))
+            ->innerjoin($db->quoteName('#__users', 'U') . 'ON P.user_id = U.id');
 
         return $db->setQuery($getProjects)->loadObjectList();
+    }
+
+    public function getProject($projectID)
+    {
+        $db = JFactory::getDbo();
+        $getProjects = $db->getQuery(true);
+
+        $columns = array(
+            'P.id',
+            'P.user_id',
+            'P.group_id',
+            'P.name',
+            'P.description',
+            'P.type_id',
+            'P.creation_date',
+            'P.activated',
+            //'U.name',
+            'U.username',
+            'U.email'
+        );
+
+        $getProjects
+            ->select($columns)
+            ->from($db->quoteName('#__pt_project', 'P'))
+            ->innerjoin($db->quoteName('#__users', 'U') . 'ON P.user_id = U.id')
+            ->where($db->quoteName('P.id') . ' = ' . $db->quote($projectID))
+            ->setLimit(1);
+
+        return $db->setQuery($getProjects)->loadAssoc();
     }
 }
