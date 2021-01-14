ad6299cf6f5e2968f588e3e85dc80179aae82905
Morgan Nolan <morgan.nolan@hotmail.com>
2021-01-14 23:31:33 +0000

    Added sidebar to pool view.

--- admin/views/pool/tmpl/default.php
+++ admin/views/pool/tmpl/default.php
@@ -1,11 +1,20 @@
 <?php defined('_JEXEC') or die; ?>
 
-<input id="token" type="hidden" name="<?php echo JSession::getFormToken(); ?>" value="1"/>
+<input id="token"
+       type="hidden"
+       name="<?php echo JSession::getFormToken(); ?>"
+       value="1"/>
 
-<div style="width: 60%; margin: 0 auto;">
-    <?php $this->questionCounter = 0; ?>
-    <?php foreach ($this->questions as $this->question): ?>
-        <?php $this->questionCounter++; ?>
-        <?php echo $this->loadTemplate('question'); ?>
-    <?php endforeach; ?>
+<div id="main">
+    <?php echo $this->loadTemplate('sidebar'); ?>
+
+    <span class="openNavigation" onclick="openNav()">&#9776; navigation</span>
+
+    <div style="width: 60%; margin: 0 auto;">
+        <?php $this->questionCounter = 0; ?>
+        <?php foreach ($this->questions as $this->question): ?>
+            <?php $this->questionCounter++; ?>
+            <?php echo $this->loadTemplate('question'); ?>
+        <?php endforeach; ?>
+    </div>
 </div>
