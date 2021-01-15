83e0d9882eb68d6ca5c68c68410b77002e5fe9e8
Morgan Nolan <morgan.nolan@hotmail.com>
2021-01-15 22:05:10 +0000

    Refactored project form.
    Preparing for delete project functionality within settings view.

--- site/views/projectcreate/tmpl/default.php
+++ site/views/projectcreate/tmpl/default.php
@@ -1,59 +1,17 @@
-<?php
-/**
- * @package     Joomla.Administrator
- * @subpackage  com_helloworld
- *
- * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- *
- * This layout file is for displaying the front end form for capturing a new helloworld message
- *
- */
+<?php defined('_JEXEC') or die('Restricted access'); ?>
+<?php JHtml::_('behavior.formvalidator'); ?>
 
-// No direct access
-defined('_JEXEC') or die('Restricted access');
-JHtml::_('behavior.formvalidator');
-
-?>
 <form action="<?php echo JRoute::_('index.php?option=com_progresstool&view=projectcreate'); ?>"
       method="post" name="adminForm" id="adminForm" class="projectForm" enctype="multipart/form-data">
 
     <div class="form-horizontal">
+        <h1>Project Details</h1>
         <fieldset class="adminform">
-            <legend>Project Creation Form</legend>
-            <?php // echo $this->form->renderFieldset('details'); ?>
-            <div class="test">
-                <?php echo $this->form->getInput('projectID'); ?>
-            </div>
-            <div class="test">
-                <?php echo $this->form->getLabel('name'); ?>
-                <?php echo $this->form->getInput('name'); ?>
-            </div>
-            <div class="test">
-                <?php echo $this->form->getLabel('description'); ?>
-                <?php echo $this->form->getInput('description'); ?>
-            </div>
-            <div class="test">
-                <?php echo $this->form->getLabel('type'); ?>
-                <?php echo $this->form->getInput('type'); ?>
-            </div>
-            <div class="test">
-                <?php echo $this->form->getLabel('group'); ?>
-                <?php echo $this->form->getInput('group'); ?>
-            </div>
+            <?php echo $this->form->renderFieldset('details'); ?>
+            <button type="button" class="submitButton" onclick="Joomla.submitbutton('projectcreate.save')">
+                Create project
+            </button>
         </fieldset>
-    </div>
-
-    <div class="buttonChest">
-        <button type="button" class="buttonCancel" onclick="Joomla.submitbutton('projectcreate.cancel')">
-            <span class="icon-cancel"></span><?php echo ' ' . JText::_('JCANCEL') ?>
-        </button>
-
-        <div class="gap"></div>
-
-        <button type="button" class="buttonSubmit" onclick="Joomla.submitbutton('projectcreate.save')">
-            <span class="icon-ok"></span><?php echo ' ' . JText::_('JSAVE') ?>
-        </button>
     </div>
 
     <input type="hidden" name="task"/>
--- site/views/settings/tmpl/default.php
+++ site/views/settings/tmpl/default.php
@@ -1,61 +1,31 @@
-<?php
-/**
- * @package     Joomla.Administrator
- * @subpackage  com_helloworld
- *
- * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- *
- * This layout file is for displaying the front end form for capturing a new helloworld message
- *
- */
+<?php defined('_JEXEC') or die('Restricted access'); ?>
+<?php JHtml::_('behavior.formvalidator'); ?>
 
-// No direct access
-defined('_JEXEC') or die('Restricted access');
-JHtml::_('behavior.formvalidator');
-
-?>
 <form action="<?php echo JRoute::_('index.php?option=com_progresstool&view=settings'); ?>"
       method="post" name="adminForm" id="adminForm" class="projectForm" enctype="multipart/form-data">
 
     <div class="form-horizontal">
+        <h1>Project Details</h1>
         <fieldset class="adminform">
-            <legend>Project Creation Form</legend>
-            <?php // echo $this->form->renderFieldset('details'); ?>
-            <div class="test">
-                <?php echo $this->form->getInput('projectID'); ?>
-            </div>
-            <div class="test">
-                <?php echo $this->form->getLabel('name'); ?>
-                <?php echo $this->form->getInput('name'); ?>
-            </div>
-            <div class="test">
-                <?php echo $this->form->getLabel('description'); ?>
-                <?php echo $this->form->getInput('description'); ?>
-            </div>
-            <div class="test">
-                <?php echo $this->form->getLabel('type'); ?>
-                <?php echo $this->form->getInput('type'); ?>
-            </div>
-            <div class="test">
-                <?php echo $this->form->getLabel('group'); ?>
-                <?php echo $this->form->getInput('group'); ?>
-            </div>
+            <?php echo $this->form->renderFieldset('details'); ?>
+            <button type="button" class="submitButton" onclick="Joomla.submitbutton('settings.update')">
+                Update project details
+            </button>
         </fieldset>
-    </div>
-
-    <div class="buttonChest">
-        <button type="button" class="buttonCancel" onclick="Joomla.submitbutton('settings.cancel')">
-            <span class="icon-cancel"></span><?php echo ' ' . JText::_('JCANCEL') ?>
-        </button>
-
-        <div class="gap"></div>
-
-        <button type="button" class="buttonSubmit" onclick="Joomla.submitbutton('settings.update')">
-            <span class="icon-ok"></span><?php echo ' ' . JText::_('JSAVE') ?>
-        </button>
     </div>
 
     <input type="hidden" name="task"/>
     <?php echo JHtml::_('form.token'); ?>
 </form>
+
+<div class="dangerZone">
+    <h1>Danger Zone</h1>
+    <div class="dangerContainer">
+        <p>
+            <b>Delete this project</b><br>Your project no longer accessible should you choose to delete it.
+        </p>
+        <button onclick="alert('I am dangerous!')">
+            Delete this project
+        </button>
+    </div>
+</div>
