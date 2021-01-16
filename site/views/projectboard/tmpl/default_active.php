780fdb54218d8de8937d9b41ac8d2e2de0529924
Morgan Nolan <morgan.nolan@hotmail.com>
2021-01-16 00:58:33 +0000

    Sizable name refactor.

--- site/views/projectboard/tmpl/default_active.php
+++ site/views/projectboard/tmpl/default_active.php
@@ -9,30 +9,24 @@ $alt = $this->projectCount % 2 == 0 ? 2 : 0;
 ?>
 
 
-<div class="projectChest" id="<?php echo $id; ?>">
+<div class="project" id="<?php echo $id; ?>">
 
-    <div class="projectDetails" style="order: <?php echo $alt; ?>;">
-        <div class="projectTitle">
-            <?php echo $title; ?>
-        </div>
-        <div class="projectType">
-            <?php echo $type; ?>
-        </div>
-        <div class="projectDescription">
-            <?php echo $description; ?>
-        </div>
+    <div class="details" style="order: <?php echo $alt; ?>;">
+        <div class="title"><?php echo $title; ?></div>
+        <div class="type"><?php echo $type; ?></div>
+        <div class="description"><?php echo $description; ?></div>
     </div>
 
-    <div class="buttonChest" style="order: <?php echo $alt == 2 ? 0 : 2; ?>;">
-        <button class="surveyButton" onclick="window.location = `?option=com_progresstool&view=survey&projectID=${<?php echo $id;?>}`">
-            Survey
-        </button>
-        <button class="metricsButton" onclick="window.location = `?option=com_progresstool&view=metrics&projectID=${<?php echo $id;?>}`">
-            Metrics
-        </button>
-        <button class="settingsButton" onclick="window.location = `?option=com_progresstool&view=settings&projectID=${<?php echo $id;?>}`">
-            Settings
-        </button>
+    <div class="buttons" style="order: <?php echo $alt == 2 ? 0 : 2; ?>;">
+        <button class="survey"
+                onclick="window.location = `?option=com_progresstool&view=survey&projectID=${<?php echo $id;?>}`">
+            Survey</button>
+        <button class="metrics"
+                onclick="window.location = `?option=com_progresstool&view=metrics&projectID=${<?php echo $id;?>}`">
+            Metrics</button>
+        <button class="settings"
+                onclick="window.location = `?option=com_progresstool&view=settings&projectID=${<?php echo $id;?>}`">
+            Settings</button>
     </div>
 
 </div>
--- site/views/projectboard/tmpl/default_inactive.php
+++ site/views/projectboard/tmpl/default_inactive.php
@@ -3,18 +3,14 @@
 $alt = $this->projectCount % 2 == 0 ? 2 : 0;
 ?>
 
-<div class="projectChest" id="<?php echo $this->project->id; ?>">
+<div class="project" id="<?php echo $this->project->id; ?>">
 
-    <div class="projectApproval" style="order: <?php echo $alt; ?>;">
-        <div class="projectTitle">
-            <?php echo $this->project->name; ?>
-        </div>
-
-        <div class="projectType">
+    <div class="approval" style="order: <?php echo $alt; ?>;">
+        <div class="title"><?php echo $this->project->name; ?></div>
+        <div class="type">
             These prerequisites should be discussed within your group at an early stage to gauge the viability of the project. Tick each question to
             indicate the points have been discussed and progress to the survey
         </div>
-
         <div class="approvalCheck">
             <?php foreach ($this->approvalQuestions as $question):
                 $param = $this->project->id . ',' . $question->id . ',' . $this->projectCount;
@@ -25,7 +21,6 @@ $alt = $this->projectCount % 2 == 0 ? 2 : 0;
                         : ''
                         : '';
                 ?>
-
                 <label class="optionChest" style="--outlineColour: #ffffff; --optionColour: #ffffff;">
                     <input class="optionInput" onclick="approvalSelect(<?php echo $param; ?>)" type="checkbox" <?php echo $checkStr; ?>>
                     <span class="optionLabel" style="--labelColour: 255, 255, 255; color: #ffffff;">
@@ -34,15 +29,12 @@ $alt = $this->projectCount % 2 == 0 ? 2 : 0;
                             </span>
                         </span>
                 </label>
-
             <?php endforeach; ?>
         </div>
     </div>
 
-    <div class="buttonChest" style="order: <?php echo $alt == 2 ? 0 : 2; ?>;">
-        <button class="approvalButton" onclick="window.location = `/oss-resources`">
-            Helpful Resources
-        </button>
+    <div class="buttons" style="order: <?php echo $alt == 2 ? 0 : 2; ?>;">
+        <button class="resources" onclick="window.location = `/oss-resources`">Helpful Resources</button>
     </div>
 
 </div>
