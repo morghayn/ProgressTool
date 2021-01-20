289c8042c4c071818c306c8df7bc57ba51a0cf80
Morgan Nolan <morgan.nolan@hotmail.com>
2021-01-20 21:52:32 +0000

    Redesigned panel.

--- admin/views/countries/tmpl/default.php
+++ admin/views/countries/tmpl/default.php
@@ -5,16 +5,13 @@
     <?php echo $this->heading; ?>
     <?php echo $this->sidebar; ?>
 
-    <div class="navigationFragments">
-        <div class="fragment">
-            <?php foreach ($this->countries as $country): ?>
-                <div class="child">
-                    <h1><?php echo $country->country; ?></h1>
-                    <a href="?option=com_progresstool&view=questions&countryID=<?php echo $country->id; ?>">Questions</a>
-                    <a href="?option=com_progresstool&view=tasks&countryID=<?php echo $country->id; ?>">Tasks</a>
-                </div>
-            <?php endforeach; ?>
-            <a class="create" href="">Create New</a>
-        </div>
+    <div class="cards">
+        <?php foreach ($this->countries as $country): ?>
+            <div class="card">
+                <h1><?php echo $country->country; ?></h1>
+                <a href="?option=com_progresstool&view=questions&countryID=<?php echo $country->id; ?>">Questions</a>
+                <a href="?option=com_progresstool&view=tasks&countryID=<?php echo $country->id; ?>">Tasks</a>
+            </div>
+        <?php endforeach; ?>
     </div>
 </div>
---
+++ media/js/admin/countries.js
@@ -0,0 +1,4 @@
+function createCountry()
+{
+    alert("I am going to be programmed to do something. o,,o")
+}
