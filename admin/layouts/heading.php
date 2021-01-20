5a0511552aa5173beaad68150afc645990404564
Morgan Nolan <morgan.nolan@hotmail.com>
2021-01-20 21:50:41 +0000

    Added function to allow additional HTML to be added dynamically.

--- admin/layouts/heading.php
+++ admin/layouts/heading.php
@@ -2,6 +2,13 @@
 
 <!-- Heading -->
 <div class="heading">
-    <span class="openNavigation" onclick="openNav()">&#9776;</span>
+    <span class="openSidebar" onclick="openNav()">&#9776;</span>
+
+    <?php if (array_key_exists('additions', $displayData)): ?>
+        <?php foreach ($displayData['additions'] as $addition): ?>
+            <?php echo $addition; ?>
+        <?php endforeach; ?>
+    <?php endif; ?>
+
     <h1><?php echo $displayData['page']; ?></h1>
 </div>
