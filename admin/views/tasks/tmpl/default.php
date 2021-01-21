3d6199245f11409a468226e535612edf8826e708
Morgan Nolan <morgan.nolan@hotmail.com>
2021-01-21 22:35:31 +0000

    Styling update to tasks.

--- admin/views/tasks/tmpl/default.php
+++ admin/views/tasks/tmpl/default.php
@@ -5,8 +5,17 @@
     <?php echo $this->heading; ?>
     <?php echo $this->sidebar; ?>
 
-    <h1>Here we will display tasks</h1>
-    <?php var_dump($this->tasks); ?>
+    <div class="tasks">
+        <?php foreach ($this->tasks as $task): ?>
+            <?php $colour = $this->categories[--$task->category_id]->colour_hex; ?>
+            <div class="task">
+                <div class="heading" style="background-color: <?php echo $colour; ?>">
+                    <span class="id">ID:<?php echo $task->id; ?></span>
+                    <h1><?php echo $task->task; ?></h1>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </div>
 
 <!-- CSRF Token -->
