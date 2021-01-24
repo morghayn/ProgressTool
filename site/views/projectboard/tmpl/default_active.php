7028650c85d132dea1fafccf39c15edcb18e67d6
Morgan Nolan <morgan.nolan@hotmail.com>
2021-01-24 14:04:51 +0000

    Refactor.

--- site/views/projectboard/tmpl/default_active.php
+++ site/views/projectboard/tmpl/default_active.php
@@ -1,32 +1,23 @@
-<?php defined('_JEXEC') or die;
+<?php defined('_JEXEC') or die; ?>
 
-$id = $this->project->id;
-$title = $this->project->name;
-$description = $this->project->description;
-$type = $this->project->type;
-$alt = $this->projectCount % 2 == 0 ? 2 : 0;
+<div class="project" id="<?php echo $this->project->id; ?>">
 
-?>
-
-
-<div class="project" id="<?php echo $id; ?>">
-
-    <div class="details" style="order: <?php echo $alt; ?>;">
-        <div class="title"><?php echo $title; ?></div>
-        <div class="type"><?php echo $type; ?></div>
-        <div class="description"><?php echo $description; ?></div>
+    <div class="details" style="order: <?php echo $this->count % 2 ? 0 : 2; ?>;">
+        <div class="title"><?php echo $this->project->name; ?></div>
+        <div class="type"><?php echo $this->project->type; ?></div>
+        <div class="description"><?php echo $this->project->description; ?></div>
     </div>
 
-    <div class="buttons" style="order: <?php echo $alt == 2 ? 0 : 2; ?>;">
-        <button class="survey"
-                onclick="window.location = `?option=com_progresstool&view=survey&projectID=${<?php echo $id;?>}`">
-            Survey</button>
-        <button class="metrics"
-                onclick="window.location = `?option=com_progresstool&view=metrics&projectID=${<?php echo $id;?>}`">
-            Metrics</button>
-        <button class="settings"
-                onclick="window.location = `?option=com_progresstool&view=settings&projectID=${<?php echo $id;?>}`">
-            Settings</button>
+    <div class="buttons" style="order: <?php echo $this->count % 2 ? 2 : 0; ?>;">
+        <button class="survey" onclick="window.location = `?option=com_progresstool&view=survey&projectID=${<?php echo $this->project->id; ?>}`">
+            Survey
+        </button>
+        <button class="metrics" onclick="window.location = `?option=com_progresstool&view=metrics&projectID=${<?php echo $this->project->id; ?>}`">
+            Metrics
+        </button>
+        <button class="settings" onclick="window.location = `?option=com_progresstool&view=settings&projectID=${<?php echo $this->project->id; ?>}`">
+            Settings
+        </button>
     </div>
 
 </div>
--- site/views/projectboard/tmpl/default_inactive.php
+++ site/views/projectboard/tmpl/default_inactive.php
@@ -1,11 +1,8 @@
-<?php defined('_JEXEC') or die;
-
-$alt = $this->projectCount % 2 == 0 ? 2 : 0;
-?>
+<?php defined('_JEXEC') or die; ?>
 
 <div class="project" id="<?php echo $this->project->id; ?>">
 
-    <div class="approval" style="order: <?php echo $alt; ?>;">
+    <div class="approval" style="order: <?php echo $this->count % 2 ? 0 : 2; ?>;">
         <div class="title"><?php echo $this->project->name; ?></div>
         <div class="type">
             These prerequisites should be discussed within your group at an early stage to gauge the viability of the project. Tick each question to
@@ -13,7 +10,7 @@ $alt = $this->projectCount % 2 == 0 ? 2 : 0;
         </div>
         <div class="approvalCheck">
             <?php foreach ($this->approvalQuestions as $question):
-                $param = $this->project->id . ',' . $question->id . ',' . $this->projectCount;
+                $param = $this->project->id . ',' . $question->id . ',' . $this->count;
                 // TODO: fix this
                 $checkStr =
                     array_key_exists($this->project->id, $this->projectApprovalSelections)
@@ -22,22 +19,20 @@ $alt = $this->projectCount % 2 == 0 ? 2 : 0;
                         : ''
                         : '';
                 ?>
-                <label class="optionChest" style="--outlineColour: #ffffff; --optionColour: #ffffff;">
-                    <input class="optionInput" onclick="approvalSelect(<?php echo $param; ?>)" type="checkbox" <?php echo $checkStr; ?>>
-                    <span class="optionLabel" style="--labelColour: 255, 255, 255; color: #ffffff;">
-                            <span class="option" style="color: #ffffff;">
-                                <?php echo $question->question; ?><span class="disclaimer">*</span>
-                            </span>
+                <label class="choice" style="--outlineColour: #ffffff; --optionColour: #ffffff;">
+                    <input onclick="approvalSelect(<?php echo $param; ?>)" type="checkbox" <?php echo $checkStr; ?>>
+                    <span class="box" style="--labelColour: 255, 255, 255; color: #ffffff;">
+                        <span class="text" style="color: #ffffff;">
+                            <?php echo $question->question; ?><span class="disclaimer">*</span>
                         </span>
+                    </span>
                 </label>
             <?php endforeach; ?>
-            <p class="disclaimer">
-                The survey will not be available until you have checked these boxes.
-            </p>
+            <p class="disclaimer">The survey will not be available until you have checked these boxes.</p>
         </div>
     </div>
 
-    <div class="buttons" style="order: <?php echo $alt == 2 ? 0 : 2; ?>;">
+    <div class="buttons" style="order: <?php echo $this->count % 2 ? 2 : 0; ?>;">
         <button class="resources" onclick="window.location = `/oss-resources`">Helpful Resources</button>
     </div>
 
