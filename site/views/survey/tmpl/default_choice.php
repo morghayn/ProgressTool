f52f5da56b08549aa335ab35f75007ff89ddc616
Morgan Nolan <morgan.nolan@hotmail.com>
2021-01-24 11:48:40 +0000

    Finished updating language used for choice styling.

--- site/views/survey/tmpl/default_choice.php
+++ site/views/survey/tmpl/default_choice.php
@@ -3,9 +3,8 @@
 <label class="choice" style="--outlineColour: <?php echo $this->colourHex; ?>; --optionColour: <?php echo $this->colourHex; ?>;">
     <input id="qcid-<?php echo $this->choiceID; ?>" type="checkbox" <?php echo $this->isChecked; ?>
            onclick="surveySelect('<?php echo $this->projectID . "','" . $this->choiceID; ?>')">
-    <span class="optionLabel" style="--labelColour: <?php echo $this->colourRGB; ?>;">
-        <span class="option">
-            <?php echo $this->choice->choice; ?>
-        </span>
+
+    <span class="box" style="--labelColour: <?php echo $this->colourRGB; ?>;">
+        <span class="text"><?php echo $this->choice->choice; ?></span>
     </span>
 </label>
