8bf2448c6f57c2d16d2b9cd0446fffbeaddb701b
Morgan Nolan <morgan.nolan@hotmail.com>
2021-01-26 04:27:18 +0000

    Tasks are no longer forms, will be going contenteditable route.

--- admin/views/tasks/tmpl/default_task.php
+++ admin/views/tasks/tmpl/default_task.php
@@ -1,11 +1,11 @@
 <?php defined('_JEXEC') or die; ?>
 
-<form id="<?php echo $this->taskid; ?>" class="task" onclick="openTaskEditor(this.id)" style="border-color: <?php echo $this->colourHex; ?>">
+<div id="<?php echo $this->taskid; ?>" class="task" onclick="openTaskEditor(this.id)" style="border-color: <?php echo $this->colourHex; ?>">
     <div class="heading" style="background-color: <?php echo $this->colourHex; ?>">
         <h2>ID:<?php echo $this->task->id; ?></h2>
-        <h1><?php echo $this->task->task; ?></h1>
+        <h1 id="task" contenteditable="true"><?php echo $this->task->task; ?></h1>
     </div>
     <div class="editor">
         <!-- Work in progress -->
     </div>
-</form>
+</div>
