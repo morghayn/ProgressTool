2bf109d06d4f890790092a3b72bb2d841da6a0b8
Morgan Nolan <morgan.nolan@hotmail.com>
2021-01-29 12:41:56 +0000

    Created statistics skeleton. Will be used as a REST end point to get statistics.

---
+++ site/controllers/statistics.json.php
@@ -0,0 +1,21 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * (Site) Class ProgressToolControllerStatistics
+ *
+ * Controller for back-end statistics functionality.
+ *
+ * @package ProgressTool
+ * @subpackage admin
+ * @since 0.5.5
+ *
+ * @author  Morgan Nolan <morgan.nolan@hotmail.com>
+ * @link    https://github.com/morghayn
+ */
+class ProgressToolControllerStatistics extends JControllerLegacy
+{
+    public function getStatistics()
+    {
+
+    }
+}
---
+++ site/models/statistics.php
@@ -0,0 +1,18 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * (Site) Class ProgressToolModelStatistics
+ *
+ * Model for front-end statistics functionality.
+ *
+ * @package ProgressTool
+ * @subpackage site
+ * @since 0.5.5
+ *
+ * @author  Morgan Nolan <morgan.nolan@hotmail.com>
+ * @link    https://github.com/morghayn
+ */
+class ProgressToolModelStatistics extends JModelItem
+{
+
+}
