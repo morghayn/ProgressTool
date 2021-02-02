3f65e05d031362f3569e7ab8286e18779f4cf188
Morgan Nolan <morgan.nolan@hotmail.com>
2021-02-02 08:13:30 +0000

    Further style tweaks.

--- admin/views/tasks/tmpl/default_task.php
+++ admin/views/tasks/tmpl/default_task.php
@@ -14,28 +14,28 @@
         <h1>Criteria: <?php echo $this->task->criteria; ?></h1>
 
         <h1>Logic</h1>
-        <div class="choices">
+        <div class="grouping">
             <label class="choice" style="--outlineColour: <?php echo $this->colourHex; ?>; --optionColour: <?php echo $this->colourHex; ?>;">
-                <input type="checkbox" <?php echo ($this->task->logic_id == 0 ? 'checked' : '');?>>
+                <input type="checkbox" <?php echo($this->task->logic_id == 0 ? 'checked' : ''); ?>>
                 <span class="box" style="--labelColour: <?php echo $this->colourRGB; ?>;">
                     <span class="text">OR</span>
                 </span>
             </label>
             <label class="choice" style="--outlineColour: <?php echo $this->colourHex; ?>; --optionColour: <?php echo $this->colourHex; ?>;">
-                <input type="checkbox" <?php echo ($this->task->logic_id == 1 ? 'checked' : '');?>>
+                <input type="checkbox" <?php echo($this->task->logic_id == 1 ? 'checked' : ''); ?>>
                 <span class="box" style="--labelColour: <?php echo $this->colourRGB; ?>;">
                     <span class="text">AND</span>
                 </span>
             </label>
         </div>
 
-
         <h1>Choices</h1>
-        <div id="choices" class="choices">
+        <div id="choices" class="grouping">
             <?php foreach ($this->task->choices as $this->choice): ?>
                 <?php $this->choiceID = 'choiceid-' . $this->choice->id; ?>
 
-                <div id="<?php echo $this->choiceID; ?>" class="choice item" style="border-color: <?php echo $this->colourHex; ?>">
+                <div style="border-color: <?php echo $this->colourHex; ?>; --hoverColour: <?php echo $this->colourRGB; ?>;"
+                     id="<?php echo $this->choiceID; ?>" class="choice item">
                     <h3 id="<?php echo $this->choice->id; ?>">CID:<?php echo $this->choice->id; ?></h3>
                     <h3>W:<?php echo $this->choice->weight; ?></h3>
                     <button onclick="removeChoice('<?php echo $this->task->id . "','" . $this->choice->id; ?>')">Remove</button>
@@ -43,7 +43,7 @@
                 </div>
             <?php endforeach; ?>
 
-            <button class="add" onclick="openModal()">Add Choice</button>
+            <button class="loose" onclick="openModal()">Add Choice</button>
         </div>
 
     </div>
