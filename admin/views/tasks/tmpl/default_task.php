d059ffe55b474eb0763a32666e99cfd013c6b0f3
Morgan Nolan <morgan.nolan@hotmail.com>
2021-02-02 08:33:59 +0000

    Minor style tweaks and re-arrangements.

--- admin/views/tasks/tmpl/default_task.php
+++ admin/views/tasks/tmpl/default_task.php
@@ -10,11 +10,25 @@
 
     <!-- Editor -->
     <div class="editor">
+        <h1>Choices</h1>
+        <div id="choices" class="grouping">
+            <?php foreach ($this->task->choices as $this->choice): ?>
+                <?php $this->choiceID = 'choiceid-' . $this->choice->id; ?>
 
-        <h1>Criteria: <?php echo $this->task->criteria; ?></h1>
+                <div style="border-color: <?php echo $this->colourHex; ?>; --hoverColour: <?php echo $this->colourRGB; ?>;"
+                     id="<?php echo $this->choiceID; ?>" class="choice item">
+                    <h3>CID:<?php echo $this->choice->id; ?></h3>
+                    <h3>W:<?php echo $this->choice->weight; ?></h3>
+                    <button onclick="removeChoice('<?php echo $this->task->id . "','" . $this->choice->id; ?>')">Remove</button>
+                    <h2><?php echo $this->choice->choice; ?></h2>
+                </div>
+            <?php endforeach; ?>
 
-        <h1>Logic</h1>
-        <div class="grouping">
+            <button class="loose" onclick="openModal()">Add Choice</button>
+        </div>
+
+        <h1>Logic | C:<?php echo $this->task->criteria;?></h1>
+        <div class="grouping" style="display: block;">
             <label class="choice" style="--outlineColour: <?php echo $this->colourHex; ?>; --optionColour: <?php echo $this->colourHex; ?>;">
                 <input type="checkbox" <?php echo($this->task->logic_id == 0 ? 'checked' : ''); ?>>
                 <span class="box" style="--labelColour: <?php echo $this->colourRGB; ?>;">
@@ -29,23 +43,7 @@
             </label>
         </div>
 
-        <h1>Choices</h1>
-        <div id="choices" class="grouping">
-            <?php foreach ($this->task->choices as $this->choice): ?>
-                <?php $this->choiceID = 'choiceid-' . $this->choice->id; ?>
-
-                <div style="border-color: <?php echo $this->colourHex; ?>; --hoverColour: <?php echo $this->colourRGB; ?>;"
-                     id="<?php echo $this->choiceID; ?>" class="choice item">
-                    <h3 id="<?php echo $this->choice->id; ?>">CID:<?php echo $this->choice->id; ?></h3>
-                    <h3>W:<?php echo $this->choice->weight; ?></h3>
-                    <button onclick="removeChoice('<?php echo $this->task->id . "','" . $this->choice->id; ?>')">Remove</button>
-                    <h2><?php echo $this->choice->choice; ?></h2>
-                </div>
-            <?php endforeach; ?>
-
-            <button class="loose" onclick="openModal()">Add Choice</button>
-        </div>
-
+        <!--<h1>Criteria: <?php //echo $this->task->criteria; ?></h1>-->
     </div>
 </div>
 
