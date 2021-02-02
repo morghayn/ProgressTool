88fde0b5ae9eee950de0555a23e990d352fd1e04
Morgan Nolan <morgan.nolan@hotmail.com>
2021-02-02 07:34:32 +0000

    Style changes.

--- admin/views/tasks/tmpl/default_task.php
+++ admin/views/tasks/tmpl/default_task.php
@@ -36,17 +36,14 @@
                 <?php $this->choiceID = 'choiceid-' . $this->choice->id; ?>
 
                 <div id="<?php echo $this->choiceID; ?>" class="choice item" style="border-color: <?php echo $this->colourHex; ?>">
-                    <button onclick="removeChoice('<?php echo $this->task->id . "','" . $this->choice->id; ?>')">Remove</button>
                     <h3 id="<?php echo $this->choice->id; ?>">CID:<?php echo $this->choice->id; ?></h3>
                     <h3>W:<?php echo $this->choice->weight; ?></h3>
+                    <button onclick="removeChoice('<?php echo $this->task->id . "','" . $this->choice->id; ?>')">Remove</button>
                     <h2><?php echo $this->choice->choice; ?></h2>
                 </div>
             <?php endforeach; ?>
 
-            <div class="buttons">
-                <button class="add" onclick="openModal()">Add Choice</button>
-                <button class="save">Save</button>
-            </div>
+            <button class="add" onclick="openModal()">Add Choice</button>
         </div>
 
     </div>
