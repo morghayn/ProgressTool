e653b50db11edb54b20f5d1eb275d6f47f1446b3
Morgan Nolan <morgan.nolan@hotmail.com>
2021-03-08 00:10:47 +0000

    Added tooltips containing project name to project plots.

--- site/views/test/tmpl/default.php
+++ site/views/test/tmpl/default.php
@@ -10,6 +10,7 @@
                          style="top: <?php echo $plot['y']; ?>%; left: calc(<?php echo $plot['x']; ?>% - 3rem); --backgroundColour: <?php echo $plot['colour_rgb']; ?>;">
                         <img src="<?php echo JURI::root() . $plot['icon_path']; ?>"
                              alt="<?php echo $plot['project_name']; ?>">
+                        <span class="name"><?php echo $plot['project_name']; ?></span>
                     </div>
                     <?php //TODO echo $category->colour_rgb; ?>
                 <?php endforeach; ?>
