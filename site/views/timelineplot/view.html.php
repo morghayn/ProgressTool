0da11877d3caade6e0bd0a5b31fc731818c7ffbe
Morgan Nolan <morgan.nolan@hotmail.com>
2021-03-08 00:29:09 +0000

    Renamed the timeline plot feature to 'timeline plot' (from 'test').

--- site/views/timelineplot/view.html.php
+++ site/views/timelineplot/view.html.php
@@ -12,7 +12,7 @@
  * @author  Morgan Nolan <morgan.nolan@hotmail.com>
  * @link    https://github.com/morghayn
  */
-class ProgressToolViewTest extends JViewLegacy
+class ProgressToolViewTimelinePlot extends JViewLegacy
 {
     protected $categories, $projects, $plots;
     private $countryID;
